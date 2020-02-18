1515c01fb247811920129261c914ec0b4d14615c
Frédéric Penel <frederic.penel@gmx.fr>
2020-02-18 12:06:00 +0100

    init categories controller

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace supermarket.sharp.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        
+        [HttpGet]
+        public int GetAllAsync()
+        {
+            return 0;
+        }
+    }
+}
